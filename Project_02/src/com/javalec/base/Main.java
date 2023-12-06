bc0d906981446b01abd2f2f0cf6a39dbd338737a
Gwangyeong Kim <rhkddud0310@naver.com>
2023-12-06 17:59:56 +0900

    first commit

---
+++ Project_02/src/com/javalec/base/Main.java
@@ -0,0 +1,55 @@
+package com.javalec.base;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/* ------------------ 예비 프로젝트 (2) ------------------
+		 < 절차형으로 개발된 Quiz를 객체형으로 변환.
+		 	*** 종료를 선택하기 전까지 계속 사용된다. *** >
+		 
+		 (결과)
+		 1. Ex1
+		 2. Ex2
+		 3. Ex3
+		 4. Ex4
+		 5. 종료
+		 
+		 원하는 결과물을 선택하세요 >>> 1
+		 */
+		System.out.println("<<< 예비 프로젝트 (2) >>>");
+		System.out.println("< 절차형으로 개발된 Quiz를 객체형으로 변환. >\n"
+				+ "*** 단, 종료를 선택하기 전까지 계속 사용된다. ****" + "\n");
+		
+		Scanner scan = new Scanner(System.in);
+		int inputNum = 0;	// 원하는 결과물 번호가 저장될 변수
+		String result = "";
+		
+		
+//		while(true) {
+//			System.out.println("1. Ex1\n2. Ex2\n3. Ex3\n4. Ex4\n5. 종료");
+//			System.out.print("원하는 결과물을 선택하세요 >>> ");
+//			inputNum = scan.nextInt();
+//			if(inputNum == 5) {
+//				break;
+//			}else if(inputNum == 1) {
+//				Ex_01 ex_01 = new Ex_01();
+//				System.out.println(ex_01.result);	// Ex_01 결과 출력 -> 수진이 누나
+//			}else if(inputNum == 2) {
+//				Ex_02 ex_02 = new Ex_02();
+//				System.out.println(ex_02.result);	// Ex_02 결과 출력 -> 수민이
+//			}else if(inputNum == 3) {
+//				Ex_03 ex_03 = new Ex_03();
+//				System.out.println(ex_03.result);	// Ex_03 결과 출력 -> 채림이
+//			}else if(inputNum == 4) {
+//				Ex_04 ex_04 = new Ex_04();
+//				System.out.println(ex_04.result);	// Ex_03 결과 출력 -> 채림이
+//			}
+//		}
+		
+		
+
+	}
+
+}
