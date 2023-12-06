414266d863580761378af6ac779452b425eff12a
Sumin Kim <411sumin@gmail.com>
2023-12-06 18:26:54 +0900

    inital commit

---
+++ Project_02/src/com/javalec/sumin/Ex_02.java
@@ -0,0 +1,40 @@
+package com.javalec.sumin;
+
+public class Ex_02 {
+	//property
+		public int num = 0; 
+		public int tot = 1; 
+
+		//Constructor 
+		public Ex_02() {
+			
+		}
+		
+
+		//Method 
+
+		public int factorial (int num) { 
+			for(int i = 1; i <= num; i++) {
+				tot *= i;
+			}
+			return tot; 
+
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
+		}
+	
+	
+	
+	
+}
