34f32128f2cb0684105e0f46b676fc8f6b1945d3
chaerim <cofl0428428@naver.com>
2023-12-07 09:40:45 +0900

    final

--- Project_02/src/com/javalec/chaerim/Ex_04.java
+++ Project_02/src/com/javalec/chaerim/Ex_04.java
@@ -1,22 +1,28 @@
 package com.javalec.chaerim;
 
+import java.util.Scanner;
+
 public class Ex_04 {
 
 	// Field
-		public long num;
-		int answer = 0;
-		
+		Scanner scan = new Scanner(System.in);
+		int num = 0;
+		public int answer = 0;
+
 		// Constructor
 		public Ex_04() {
 			// TODO Auto-generated constructor stub
 		}
-		
+
 		// Method
-		public long sumCalc(long num) {
-			while(num > 0) {
-			answer += (num % 10);
-			num /= 10;
+		public long sumCalc() {
+			System.out.print("Enter an integer(0 ~ 9) : ");
+			num = scan.nextInt();
+			while (num > 0) {
+				answer += (num % 10);
+				num /= 10;
 			}
+			System.out.println(answer);
 			return answer;
 		}
 }
