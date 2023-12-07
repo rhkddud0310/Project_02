1d61acf567a97681889906f672dba534601e76a4
Sumin Kim <411sumin@gmail.com>
2023-12-07 09:37:56 +0900

    final commit

--- Project_02/src/com/javalec/sumin/Ex_02.java
+++ Project_02/src/com/javalec/sumin/Ex_02.java
@@ -1,40 +1,30 @@
 package com.javalec.sumin;
 
+import java.util.Scanner;
+
 public class Ex_02 {
-	//property
-		public int num = 0; 
-		public int tot = 1; 
+	// property
+	Scanner scan = new Scanner(System.in);
+	public int num = 0;
+	public int tot = 1;
+	
+	// Constructor
+	public Ex_02() { 
 
-		//Constructor 
-		public Ex_02() {
-			
+	}
+
+	// Method
+
+	public int factorial() {
+		System.out.print("Input your decimal no. : ");
+		num = scan.nextInt();
+		for (int i = 1; i <= num; i++) {
+			tot *= i;
 		}
+		System.out.println(num + "'s factorial value = " + tot);
+		return tot;
 		
 
-		//Method 
+	}
 
-		public int factorial (int num) { 
-			for(int i = 1; i <= num; i++) {
-				tot *= i;
-			}
-			return tot; 
-
-		
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-
-	
-		}
-	
-	
-	
-	
 }
