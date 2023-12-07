94cdc749d0820a580a40f2a6d3119f3fc7f61f72
Sumin Kim <411sumin@gmail.com>
2023-12-07 10:20:29 +0900

    final

--- Project_02/src/com/javalec/sumin/Ex_02.java
+++ Project_02/src/com/javalec/sumin/Ex_02.java
@@ -3,6 +3,25 @@ package com.javalec.sumin;
 import java.util.Scanner;
 
 public class Ex_02 {
+	
+	
+//	
+//	 Description: Write an algorithm to find the factorial of a given number. 
+//	
+//	Date: 2023.12.06 (V.1)
+//	 	   2023.12.07 (V.2)
+//	
+//	 Author: Sumin Kim (김수민) 
+//	 
+//	 History: 1.) Created an instance of a Scanner class. 
+//	 		  2.) Ask the user to enter the input number.
+//	 		  3.) Declare a variable to store a number. 
+//	 		  4.) Declare a new variable for the loop and store the factorial of a number. 
+//	 		  5.) Update the factorial variable by multiplying it with the loop variable in each iteration. 
+//	 		  6.) Print the factorial of the given number. 
+//	 	
+	
+	
 	// property
 	Scanner scan = new Scanner(System.in);
 	public int num = 0;
