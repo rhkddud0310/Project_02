03f4b8e5619be812e16388667e4de3f5b8ac8fd3
Gwangyeong Kim <rhkddud0310@naver.com>
2023-12-07 10:26:07 +0900

    Main 수정 완료

--- Project_02/src/com/javalec/base/Main.java
+++ Project_02/src/com/javalec/base/Main.java
@@ -10,6 +10,22 @@ import com.javalec.sumin.Ex_02;
 public class Main {
 
 	public static void main(String[] args) {
+		/* -------------------------------------------------------------
+		(1) Desc : 절차형으로 개발된 Quiz를 객체형으로 변환하는 프로그램 만들기.
+		(2) Date : 2023.12.06
+		(3) Author : Gwangyeong Kim
+        (4) History
+        	1) 최초 작성
+        	2) 결과물 구분해주기 위한 구분선 추가
+        		1. System.out.println("------------\n");
+        	3) 결과물 출력 Process 변경
+        		1. System.out.println(ex_02.result); -> ex_02.factorial();
+        		2. System.out.println(ex_03.result); -> ex_03.multiple();
+        		3. System.out.println(ex_04.result); -> ex_04.sumCalc();
+        	4) 종료 문구 추가
+        		1. System.out.println("<<< 종료 버튼을 선택하셨습니다.\n프로그램이 종료되었습니다. >>>");
+		------------------------------------------------------------- */
+		
 		/* ------------------ 예비 프로젝트 (2) ------------------
 		 < 절차형으로 개발된 Quiz를 객체형으로 변환.
 		 	*** 종료를 선택하기 전까지 계속 사용된다. *** >
@@ -23,6 +39,7 @@ public class Main {
 		 
 		 원하는 결과물을 선택하세요 >>> 1
 		 */
+		
 		System.out.println("<<< 예비 프로젝트 (2) >>>");
 		System.out.println("< 절차형으로 개발된 Quiz를 객체형으로 변환. >\n"
 				+ "*** 단, 종료를 선택하기 전까지 계속 사용된다. ***" + "\n");
